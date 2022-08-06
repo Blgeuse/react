b02a9a4e9bd014564c5dec2362788ae9350ee1ba
Blgeuse <akhmed.r4mazanov@yandex.ru>
2022-08-06 14:00:26 +0300

    dynamic rendering of classes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
 const Counter = () => {
-  const count = 0;
+  const count = 2;
   const formatCount = count === 0 ? 'empty': count;
+
+  const getBageClasses = () => {
+    let classes = "badge m-2 ";
+    classes += count === 0 ? "bg-warning" : "bg-primary";
+    return classes;
+  }
+
   return (
     <>
-      <span className='badge bg-primary m-2'>{formatCount}</span>
+      <span className={getBageClasses()}>{formatCount}</span>
       <button className='btn btn-primary btn-sm m-2'>+</button>
     </>);
 }
