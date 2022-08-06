c01d208292f6a3065eb0c9f8c3b7d5750fe27196
Blgeuse <akhmed.r4mazanov@yandex.ru>
2022-08-06 14:04:48 +0300

    event handling

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 
 const Counter = () => {
-  const count = 2;
+  let count = 2;
   const formatCount = count === 0 ? 'empty': count;
 
   const getBageClasses = () => {
     let classes = "badge m-2 ";
     classes += count === 0 ? "bg-warning" : "bg-primary";
     return classes;
+  };
+
+  const handleIncrement = () => {
+    count += 1;
   }
 
   return (
     <>
       <span className={getBageClasses()}>{formatCount}</span>
-      <button className='btn btn-primary btn-sm m-2'>+</button>
+      <button className='btn btn-primary btn-sm m-2' onClick={handleIncrement}>+</button>
     </>);
 }
 
