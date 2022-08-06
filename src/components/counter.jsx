3f10d7dd7c27b56101b0b3cb9ddbd9455ddb4478
Blgeuse <akhmed.r4mazanov@yandex.ru>
2022-08-06 14:22:37 +0300

    rendering a list from an array

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,7 +1,10 @@
 import React,{useState} from 'react';
 
 const Counter = () => {
-  const [counter, setCounter] = useState(0)
+  const [counter, setCounter] = useState(0);
+  const [tags, setTags] = useState(['tag1', 'tag2', 'tag3']);
+
+
   const formatCount = counter === 0 ? 'empty': counter;
 
   const getBageClasses = () => {
@@ -16,9 +19,20 @@ const Counter = () => {
   const handleDecrement = () => {
     setCounter(counter - 1);
   }
+  const handleTagChange = () => {
+    setTags()
+  }
 
   return (
     <>
+      <ul>
+        {tags.map(tag =>
+          (<li
+            className='btm btm-primary btm-sm m-2'
+            onclick={handleTagChange}
+            key={tag}
+          >tag</li>))}
+      </ul>
       <span className={getBageClasses()}>{formatCount}</span>
       <button
         className='btn btn-primary btn-sm m-2'
