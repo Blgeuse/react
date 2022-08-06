495c98404e13049fa4963d6c08dfbf84fb784968
Blgeuse <akhmed.r4mazanov@yandex.ru>
2022-08-06 13:55:16 +0300

    setting attributes

---
+++ src/components/counter.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Counter = () => {
+  const count = 0;
+  const formatCount = count === 0 ? 'empty': count;
+  return (
+    <>
+      <span className='badge bg-primary m-2'>{formatCount}</span>
+      <button className='btn btn-primary btn-sm m-2'>+</button>
+    </>);
+}
+
+export default Counter;
