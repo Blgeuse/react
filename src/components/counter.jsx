67d41162bdef6f35d94dcb2794b6970d631b2042
Blgeuse <akhmed.r4mazanov@yandex.ru>
2022-08-06 15:37:22 +0300

    init app

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,56 +1,39 @@
 import React,{useState} from 'react';
+import api from '../api/index'
+const Users = () => {
+  const [users, setUsers] = useState(api.users.fetchAll());
 
-const Counter = () => {
-  const [counter, setCounter] = useState(0);
-  const [tags, setTags] = useState(['tag1', 'tag2', 'tag3']);
-
-
-  const formatCount = counter === 0 ? 'empty': counter;
-
-  const getBageClasses = () => {
-    let classes = "badge m-2 ";
-    classes += counter === 0 ? "bg-warning" : "bg-primary";
-    return classes;
-  };
-
-  const handleIncrement = () => {
-    setCounter(counter + 1);
-  }
-  const handleDecrement = () => {
-    setCounter(counter - 1);
-  }
-  const handleTagChange = (deleteTag) => {
-    console.log(deleteTag);
-    setTags((prevState) => {
-      return prevState.filter(tag => tag !== deleteTag)
-    })
-  }
-
-  const renderTags = () => {
-    if (tags.length === 0) return 'no Tags'
-    return tags.map(tag =>
-      (<li
-        className='btn btn-primary btn-sm m-2'
-        onClick={() => handleTagChange(tag)}
-        key={tag}
-      >{tag}</li>))
-  }
 
   return (
-    <>
-      <ul>
-        {renderTags()}
-      </ul>
-      <span className={getBageClasses()}>{formatCount}</span>
-      <button
-        className='btn btn-primary btn-sm m-2'
-        onClick={handleIncrement}
-      >+</button>
-      <button
-        className='btn btn-primary btn-sm m-2'
-        onClick={handleDecrement}
-      >-</button>
-    </>);
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">First</th>
+        <th scope="col">Last</th>
+        <th scope="col">Handle</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+      </tr>
+      <tr>
+        <th scope="row">2</th>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>@fat</td>
+      </tr>
+      <tr>
+        <th scope="row">3</th>
+        <td colspan="2">Larry the Bird</td>
+        <td>@twitter</td>
+      </tr>
+    </tbody>
+  </table>)
 }
 
-export default Counter;
+export default Users;
