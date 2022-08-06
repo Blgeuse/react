57c3d4de34bb341494cd269be236c5ad572dd920
Blgeuse <akhmed.r4mazanov@yandex.ru>
2022-08-06 14:14:09 +0300

    useState

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,23 +1,33 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 const Counter = () => {
-  let count = 2;
-  const formatCount = count === 0 ? 'empty': count;
+  const [counter, setCounter] = useState(0)
+  const formatCount = counter === 0 ? 'empty': counter;
 
   const getBageClasses = () => {
     let classes = "badge m-2 ";
-    classes += count === 0 ? "bg-warning" : "bg-primary";
+    classes += counter === 0 ? "bg-warning" : "bg-primary";
     return classes;
   };
 
   const handleIncrement = () => {
-    count += 1;
+    setCounter(counter + 1);
+  }
+  const handleDecrement = () => {
+    setCounter(counter - 1);
   }
 
   return (
     <>
       <span className={getBageClasses()}>{formatCount}</span>
-      <button className='btn btn-primary btn-sm m-2' onClick={handleIncrement}>+</button>
+      <button
+        className='btn btn-primary btn-sm m-2'
+        onClick={handleIncrement}
+      >+</button>
+      <button
+        className='btn btn-primary btn-sm m-2'
+        onClick={handleDecrement}
+      >-</button>
     </>);
 }
 
